3c99eb5ea2121ddd6837bbe73b88935a0149278a
0x2aae086b <anonymous@TempEMail.net>
2014-07-24 05:05:55 +0000

    Add license

--- stg.lua
+++ stg.lua
@@ -1,3 +1,11 @@
+--[[
+   This program is free software. It comes without any warranty, to
+   the extent permitted by applicable law. You can redistribute it
+   and/or modify it under the terms of the Do What The Fuck You Want
+   To Public License, Version 2, as published by Sam Hocevar. See
+   the COPYING file for more details.
+]]--
+
 MODULE_HELP_CONTENTS = [[<TI><a href="event:Keys">Keys</a>
 <a href="event:Commands">Commands</a>
 <a href="event:Shot types">Shot types</a>
